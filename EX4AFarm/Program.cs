8d300752b45642b20d67997916055b35f4adfa40
Matthew <Allenm40@my.erau.edu>
2019-07-22 16:21:26 -0400

    Updated with comments.

--- EX4AFarm/Program.cs
+++ EX4AFarm/Program.cs
@@ -2,6 +2,13 @@
 
 namespace EX4AFarm
 {
+    //Created By: Matthew Allen
+    //For: ISTA 220 Exercise 4A
+    //On: July 22, 2019
+
+    /* The purpose of this program is to get more familiar with using instances of classes.
+     * I created four animals and gave them methods of Speak, Name, Product, and Food. The Name method in each class takes
+     in one string argument that it uses to print the name of the animal. */
     class Program
     {
         static void Main(string[] args)
